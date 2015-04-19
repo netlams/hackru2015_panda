fa016a252e8ad1270d434811871cfae33e80e0ea
Dau Lam <dlam@temple.edu>
2015-04-19 03:27:02 -0400

    added css

--- js/main.js
+++ js/main.js
@@ -10,27 +10,27 @@ function generateLocation() {
 	}
 }
 
-function get_tweets(dies, loc){
+// function get_tweets(dies, loc){
 
-	geocoder = new google.maps.Geocoder();
-	var lat;
-	var lon;
-	geocoder.geocode( { address: loc}, function(results, status) {
-		if(status === google.maps.GeocoderStatus.OK) {
-			lat = results[0].geometry.location.lat();
-			lon = results[0].geometry.location.lng();
-		} else {
-			alert("DANGER DANGER ERROR ERROR");
-		}
-	}
-	var url = "https://api.twitter.com/1.1/search/tweets.json?q=" + die +
-				"&geo=" + lat + "," + lon + "2mi" + "&count=100";
-	var tw_ids = [];
-	$.get(url, function(tweets_json){
-		var tweets = JSON.parse(tweets_json);
-		for(obj in tweets.statuses){
-		tw_ids.push(tweets.statuses[obj].id_str);
-	}
-		console.log(tw_ids);
-	});
-}
+// 	geocoder = new google.maps.Geocoder();
+// 	var lat;
+// 	var lon;
+// 	geocoder.geocode( { address: loc}, function(results, status) {
+// 		if(status === google.maps.GeocoderStatus.OK) {
+// 			lat = results[0].geometry.location.lat();
+// 			lon = results[0].geometry.location.lng();
+// 		} else {
+// 			alert("DANGER DANGER ERROR ERROR");
+// 		}
+// 	}
+// 	var url = "https://api.twitter.com/1.1/search/tweets.json?q=" + die +
+// 				"&geo=" + lat + "," + lon + "2mi" + "&count=100";
+// 	var tw_ids = [];
+// 	$.get(url, function(tweets_json){
+// 		var tweets = JSON.parse(tweets_json);
+// 		for(obj in tweets.statuses){
+// 		tw_ids.push(tweets.statuses[obj].id_str);
+// 	}
+// 		console.log(tw_ids);
+// 	});
+// }
