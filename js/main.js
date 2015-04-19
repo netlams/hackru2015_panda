7a7da0767494ef78829a88331cc7897bcdf750ab
Dau Lam <dlam@temple.edu>
2015-04-19 04:02:52 -0400

    google api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+function initialize() {
+    var address = (document.getElementById('searchLocation'));
+    var autocomplete = new google.maps.places.Autocomplete(address);
+    autocomplete.setTypes(['geocode']);
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+
+    var address = '';
+    if (place.address_components) {
+        address = [ (place.address_components[0] && place.address_components[0].short_name || ''),
+            		(place.address_components[1] && place.address_components[1].short_name || ''),
+            		(place.address_components[2] && place.address_components[2].short_name || '') ].join(' ');
+     }
+  	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
 function generateLocation() {
 	var address = document.getElementById('searchLocation').value;
 	if (address.length == 0 || address == null) {
@@ -9,6 +30,7 @@ function generateLocation() {
     	document.getElementById("map-canvas").innerHTML = '<iframe width="400" height="500" frameborder="0" style="border:0" src="' + web_src + '"></iframe>';
 	}
 }
+
 
 // function get_tweets(dies, loc){
 
