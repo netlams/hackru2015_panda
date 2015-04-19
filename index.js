6f4e06432ca2f0e64d7456ba0e437bc04668f007
mweidner5 <mweidner5@gmail.com>
2015-04-19 09:50:22 -0400

    Twitter api sort works. It works enough

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var twi = new Twit({
 		consumer_key: 		'FAmfLdDQXRNpXwCyNz73O3C0X',
 		consumer_secret: 	'2rcGSjL05lFSfnqn58X9KgKv777vSdj1YfjOXlb2oJGVglBywy',
 		access_token: 		'3181870612-zr0lY7Kt7jmOyUrpVYI0tVkY4f8BAlm2SfQLfJW',
-		access_token_secret: '3181870612-zr0lY7Kt7jmOyUrpVYI0tVkY4f8BAlm2SfQLfJW'  
+		access_token_secret: 'za8PWrY72Jb60JIp7BbxjInVGsLYT2gYUKKpZnp83PS6q'  
 	});
 
 app.use(bodyParser.urlencoded({
@@ -15,7 +15,18 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/', function(req, res) {
-	res.send('lat: ' + req.body.lat);
+	twi.get('search/tweets', {  q: req.body.search,
+	                            geocode: "40.0047528,-75.1180329,2mi",
+	                            count: 10 
+	                        },
+	            function(err, data){
+					console.log(req.body.search);                 
+					console.log(data);
+					for(var i=0; i<data.statuses.length; i++){
+						console.log(data.statuses.id_str);
+	                }             
+	             }
+	        );
 });
 
 app.get('/', function(req, res) {
