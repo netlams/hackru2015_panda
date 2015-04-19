c5ac1dd1cd9378276fbc35c55eeef951e746b635
mweidner5 <mweidner5@gmail.com>
2015-04-18 21:58:55 -0400

    Modified main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 
 
 function get_tweets(die, loc, count){
-	
+
+	navigator.geolocation.getCurrentPosition(function(pos){
+
+		var lat = pos.coords.latitude;
+		var lon = pos.coords.longitude;
+
+		var url = "https://api.twitter.com/1.1/search/tweets.json?q=" + die +
+					"&geo=" + lat + "," + lon + "2mi" + "&count=100";
+
+		var tw_ids = [];
+
+		$.get(url, function(tweets){
+			for(id_num in tweets.statuses.);
+		})
+
 }
